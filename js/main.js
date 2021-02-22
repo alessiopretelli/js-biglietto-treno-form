1ad6e3a89494426a05226fd4091215d06fa3325b
Alessio <ale79631@gmail.com>
2021-02-22 20:02:20 +0100

    miglioramento

--- js/main.js
+++ js/main.js
@@ -6,8 +6,6 @@ genera.addEventListener("click",
 
 
         costante = 0.21;
-        appear = document.getElementById("display_disappear");
-        appear.style.display = "block";
         nome = document.getElementById("text").value;
         document.getElementById("nome").innerHTML = nome;
         eta = document.getElementById("age").value;
@@ -16,30 +14,38 @@ genera.addEventListener("click",
         console.log(chilometri);
         document.getElementById("carrozza_num").innerHTML = (Math.floor(Math.random() * 10) + 1);
         document.getElementById("cp").innerHTML = (Math.floor(Math.random() * 10000) + 90000);
-
-
-        if (eta == "underage") {
-            costo = chilometri * costante;
-            sconto = costo * 0.2;
-            costo = costo - sconto;
-            costo = Math.round( costo * 100 + Number.EPSILON ) / 100;
-            document.getElementById("costo_biglietto").innerHTML = costo + " Eur.";
-            document.getElementById("sconto").innerHTML = "Sconto Minori";
+        
+        if ((!document.getElementById("text").value) || (!document.getElementById("km").value) || (document.getElementById("km").value > 1500)) {
+            alert("Prego, inserire tutti i dati, o i dati corretti, per proseguire con il calcolo del costo del biglietto.");
             
-        } else if (eta == "elder") {
-            costo = chilometri * costante;
-            sconto = costo * 0.4;
-            costo = costo - sconto;
-            costo = Math.round( costo * 100 + Number.EPSILON ) / 100;
-            document.getElementById("costo_biglietto").innerHTML = costo + " Eur.";
-            document.getElementById("sconto").innerHTML = "Sconto Anziani";
-
         } else {
-            costo = chilometri * costante;
-            costo = Math.round( costo * 100 + Number.EPSILON ) / 100;
-            document.getElementById("costo_biglietto").innerHTML = costo + " Eur.";
-            document.getElementById("sconto").innerHTML = "Null";
-
+            appear = document.getElementById("display_disappear");
+            appear.style.display = "block";
+    
+            if (eta == "underage") {
+                costo = chilometri * costante;
+                sconto = costo * 0.2;
+                costo = costo - sconto;
+                costo = Math.round( costo * 100 + Number.EPSILON ) / 100;
+                document.getElementById("costo_biglietto").innerHTML = costo + " Eur.";
+                document.getElementById("sconto").innerHTML = "Sconto Minori";
+                
+            } else if (eta == "elder") {
+                costo = chilometri * costante;
+                sconto = costo * 0.4;
+                costo = costo - sconto;
+                costo = Math.round( costo * 100 + Number.EPSILON ) / 100;
+                document.getElementById("costo_biglietto").innerHTML = costo + " Eur.";
+                document.getElementById("sconto").innerHTML = "Sconto Anziani";
+    
+            } else {
+                costo = chilometri * costante;
+                costo = Math.round( costo * 100 + Number.EPSILON ) / 100;
+                document.getElementById("costo_biglietto").innerHTML = costo + " Eur.";
+                document.getElementById("sconto").innerHTML = "Null";
+    
+            }
+    
         }
     }
 
